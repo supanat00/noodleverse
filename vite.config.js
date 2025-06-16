f1a715f68e1bb1d402c6b09eb58e292473d56e8c
supanat00 <supanat90@gmail.com>
2025-06-17 01:59:19 +0700

    pwa-update-101

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.png"],
+  manifest: {
+    name: "MAMA Noodle Verse",
+    short_name: "MAMA Noodle Verse",
+    description: "MAMA Noodle Verse",
+    icons: [
+      {
+        src: "./pwa/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "./pwa/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "./pwa/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "./pwa/icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "./pwa/icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+        purpose: "icon",
+      },
+    ],
+    theme_color: "#181818",
+    background_color: "#e8eac2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
